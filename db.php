8b8448309ef1a7f9fbe7c8bb896cf7bbb5bbf2c6
Carter Thayer <carterwthayer@gmail.com>
2013-10-19 11:40:55 -0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbConnect = mysqli_connect("localhost", "carterth_pie", "c00ver", "carterth_pieproject");
+	
+#mysqli_query($dbConnect, "INSERT INTO users (name) VALUES ('carter')");
+$comments_table = "comments";
+$milestones_table = "milestones";
+$projects_table ="projects";
+$user_projects_table = "user_projects";
+$users_table = "users";
+
+?>
