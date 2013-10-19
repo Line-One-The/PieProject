130ce06db332d5f0dd7de5c8fe94c4b9c7f6cf21
benbrimeyer <benjaminbrimeyer@gmail.com>
2013-10-19 13:09:18 -0500

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Pie Project</title>
+        <meta name="description" content="Do your pie projects.">
+        <meta name="viewport" content="width=device-width">
+
+        <link rel="stylesheet" href="css/global.css">
+		
+    </head>
+	
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+	
+		<div id="header">
+			<h1> <img src="images/ProjectPie.png" /> </h1>
+		</div>
+	
+		<div id="main">
+		
+			<div id="container">
+				<div id="title" class="greyBack">
+					<h1>"Welcome ".$username."!"</h1>
+				</div>
+			
+				<div id="newProject" class="greyBack">
+					<p><a href="login.html">Create new project</a></p>
+				</div>
+				
+				<div id="projects" class="greyBack">
+					<h1>Current Projects</h1>
+					
+					<table>
+						<tr><td><h2><a href="login.html">Save the world</a></h2></td></tr>
+						<tr class="subtitle"><td>this is subtitle text...</td></tr>
+					</table>
+					
+				</div>
+				
+			</div>
+		</div>
+		
+        
+    </body>
+</html>
